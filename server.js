11962c8d0fee61dd5580c82e121364e45ef50264
kyh <haa3163314@naver.com>
2021-03-05 10:16:40 +0900

    v0.1 테스트초기

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+
+app.set('view engine', 'ejs') // ejs 파일을 열수있게 한다.
+app.use(express.static('public')) 
+
+app.get('/',(req,res) =>{
+    res.render("main",{title : "voiceChat"})
+}) //경로가 "/" 일때 main.ejs가 열리고 타이틀에 이름을 변경
+
+app.get('/voiceroom',(req,res)=>{
+    res.redirect(`/voiceroom=${uuidV4()}`)
+}) //경로가 "/voiceroom일때" 경로를 "/voiceroom= + 가상 키 "가 경로로 설정된다. 
+
+app.get('/voiceroom:room',(req,res)=>{
+    res.render('room',{ roomId: req.params.room})
+    
+}) //변경된 경로에 room에 파라미터 값을 roomId에 저장 시킨다.
+
+
+
+io.on('connection', socket =>{
+    socket.on('join-room', (roomId, userId) =>{
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit("user-connected", userId)
+
+        socket.on('disconnect',() =>{
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+server.listen(3010,()=>console.log("connect port 3010"))
