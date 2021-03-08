8dadb425efcea2053098f3a90a8c95d127fbb6f5
kyh <haa3163314@naver.com>
2021-03-08 17:11:16 +0900

    v0.3 소스코드 정리

--- public/script.js
+++ public/script.js
@@ -1,135 +1,181 @@
-const socket = io('/')
-const videoGrid = document.getElementById("video-grid")
-const screen = document.getElementById("screen")
-const peer = new Peer()
-let bool = true
-let bools = true
+const socket = io('/');
+const videoArea = document.getElementById("video-grid");
+const screenArea = document.getElementById("screen");
+const peerServer = new Peer();
+let shareScreenBool = true;
 
-const myVideo = document.getElementById("myVideo")   
-const recordScreen = document.getElementById("recordVideo") //비디오 태그 생성
-const startRecord = document.getElementById("startRecord")
-const stopRecord = document.getElementById("stopRecord")
-myVideo.muted = true
-const peers = {}    // 나간 카메라에 userId를 받아 저장
+const myVideo = document.getElementById("myVideo");//자신의 웹캠 영성
+const openCamBtn = document.getElementById("openCam")//버튼클릭시 캠활성화
+const recordScreenArea = document.getElementById("recordVideo"); //비디오 태그 생성
+const startRecordBtn = document.getElementById("startRecord");
+const stopRecordBtn = document.getElementById("stopRecord");
+myVideo.muted = true;
+const peers = {};    // 나간 카메라에 userId를 받아 저장
     
 
+// 캠 영상을 녹화하는 함수 
+startRecordBtn.onclick =() =>{recordVideo(), recordScreen()};
+    
 
 const recordVideo = () =>{                                
     navigator.mediaDevices.getUserMedia({video:true, audio:false}).then(stream =>{
-    const options =  {
-      mimeType : 'video/webm; '
-    }
-    let Recorder = new MediaRecorder(stream, options)
+    const options =  { mimeType : 'video/webm; codecs=vp9 '};
+    let SetRecorder = new MediaRecorder(stream, options);
     
-    startRecord.onclick =() =>{
-        Recorder.start()
-        console.log(Recorder)
-    }
     
-    stopRecord.onclick = () =>{
-        Recorder.stop()
-        console.log(Recorder)
+        SetRecorder.start();
+        console.log(SetRecorder);
+    
+        
+    stopRecordBtn.onclick = () =>{
+        SetRecorder.stop();
+        console.log(SetRecorder);
+        myVideo.stop()
+
         const handleVideoData = (e) => {
             // blob 이벤트에서 data 추출
             const { data } = e;
             console.log(data,e)
-            // 다운로드를 위해 a 태그를 만들어주고 href로 해당 data를 다운로드 받을 수 있게 url을 만듭시다
+            // 다운로드를 위해 a 태그를 만들어주고 href로 해당 data를 다운로드 받을 수 있게 url을 만든다.
             const videoDownloadlink = document.createElement("a");
             videoDownloadlink.href = URL.createObjectURL(data);
             
-            // 다운로드 되는 파일의 이름. 확장자는 mp4 등 다양하게 가능하지만 오픈 소스인지 확인 합시다
-            link.download = "recorded.webm";
+            // 다운로드 되는 파일의 이름. 확장자는 mp4 등 다양하게 가능하지만 오픈 소스인지 확인 한다.
+            videoDownloadlink.download = "recorded.webm";
             
-            // body에 append 해줘야겠죠
-            document.body.appendChild(link);
+            // body에 append 해줌.
+            document.body.appendChild(videoDownloadlink);
             
-            // faking click. body에 append 했으니 클릭해서 다운로드를 해줘야 합니다.
-            link.click();
+            // faking click. body에 append 했으니 클릭해서 다운로드를 해줘야 한다.
+            videoDownloadlink.click();
           };
-          Recorder.addEventListener("dataavailable", handleVideoData);
+          SetRecorder.addEventListener("dataavailable", handleVideoData);
+    };
     }
-    })
-}
+    )};
+
+//     //화면공유 녹화
+//     const recordScreen = () =>{                                
+//         navigator.mediaDevices.getDisplayMedia({video:true, audio:false}).then(stream =>{
+//         const options =  { mimeType : 'video/webm; codecs=vp9 '};
+//         let SetRecorder = new MediaRecorder(stream, options);
+        
+        
+//             SetRecorder.start();
+//             console.log(SetRecorder);
+        
+        
+//         stopRecordBtn.onclick = () =>{
+//             SetRecorder.stop();
+//             console.log(SetRecorder);
+    
+    
+//             const handleVideoData = (e) => {
+//                 // blob 이벤트에서 data 추출
+//                 const { data } = e;
+//                 console.log(data,e)
+//                 // 다운로드를 위해 a 태그를 만들어주고 href로 해당 data를 다운로드 받을 수 있게 url을 만든다.
+//                 const videoDownloadlink = document.createElement("a");
+//                 videoDownloadlink.href = URL.createObjectURL(data);
+                
+//                 // 다운로드 되는 파일의 이름. 확장자는 mp4 등 다양하게 가능하지만 오픈 소스인지 확인 한다.
+//                 videoDownloadlink.download = "recorded.webm";
+                
+//                 // body에 append 해줌.
+//                 document.body.appendChild(videoDownloadlink);
+                
+//                 // faking click. body에 append 했으니 클릭해서 다운로드를 해줘야 한다.
+//                 videoDownloadlink.click();
+//               };
+//               SetRecorder.addEventListener("dataavailable", handleVideoData);
+//         };
+//     });
+// }
 
 
+// 화면공유 시켜주는 함수
 const  displayShow = () =>{
-    if(bool === true){
+    if(shareScreenBool === true){
+        shareScreenBool = false
     navigator.mediaDevices.getDisplayMedia({
         video : true
 }).then(stream =>{
-    const display = document.createElement('video')
-    screenShot(display, stream)
-})
-}else{
-    bool = false
-    display.destroy()
+    const display = document.createElement('video');
+    AddScreenShot(display, stream);
+});
 }
-} // 화면공유 시켜주는 함수
+}; 
 
-const screenShot = (video,kind) =>{
-    video.srcObject = kind
+// 화면공유를 실행할때 비디오로 나오게 해주는 함수
+const AddScreenShot = (video,kind) =>{
+    video.srcObject = kind;
     video.addEventListener('loadedmetadata',() =>{
-        video.play()
+        video.play();
         bool = false;
-    })
-    screen.append(video)
-}// 화면공유를 실행할때 비디오로 나오게 해주는 함수
+    });
+    screenArea.append(video);
+};
 
 navigator.mediaDevices.getUserMedia({
     video : true
 }).then(stream =>{ 
-    addVideoStream(myVideo, stream)
 
-    peer.on('call', call =>{
-        call.answer(stream)
-        const video = document.createElement('video')
+    openCamBtn.onclick = () =>{
+    addVideoStream(myVideo, stream);
+}
+
+//자신이 방에 들어 왔을때 추가되는 태그와 영상
+    peerServer.on('call', call =>{
+        call.answer(stream);
+        const video = document.createElement('video');
         call.on('stream', userVideoStream=>{
-            addVideoStream(video, userVideoStream)
-        })
-    }) //자신이 방에 들어 왔을때 추가되는 태그와 영상
+            addVideoStream(video, userVideoStream);
+        });
+    }); 
 
+    //상대방이 들어왔을때 userId와 영상을 새로 생성 하는 소켓
     socket.on('user-connected', userId =>{
-        connectToNewUser(userId, stream)
-    })      //상대방이 들어왔을때 userId와 영상을 새로 생성 하는 소켓
-}) 
+        connectToNewUser(userId, stream);
+    });      
+}); 
 
-
+//상대방이 방에서 나갔을때 영상을 종료하는 소켓
 socket.on("user-disconnected", userId =>{
-    console.log(userId)
-    if( peers[userId]) {peers[userId].close()}
-})//상대방이 방에서 나갔을때 영상을 종료하는 소켓
+    console.log(userId);
+    if( peers[userId]) {peers[userId].close();}
+});
 
 
+//경로를 타고왔을때 받은 KEY와 id를 서버에 보냄
+peerServer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
 
-peer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id)
-})//경로를 타고왔을때 받은 KEY와 id를 서버에 보냄
-
+//자신이 방에 들어 왔을때 추가되는 태그와 영상
 const connectToNewUser = (userId, stream) =>{
-    const call = peer.call(userId, stream)
-    const video = document.createElement('video')
+    const call = peerServer.call(userId, stream);
+    const video = document.createElement('video');
     call.on('stream', userVideoStream =>{
-        addVideoStream(video, userVideoStream)
+        addVideoStream(video, userVideoStream);
     })
     call.on('close', () =>{
-        video.remove()
+        video.remove();
     })
 
-    peers[userId] = call
-}//자신이 방에 들어 왔을때 추가되는 태그와 영상
+    peers[userId] = call;
+};
 
-
+//영상에 매체와 실행을 맡는 함수
 const addVideoStream = (video,stream) => {
-        video.srcObject = stream  
+        video.srcObject = stream;  
         video.addEventListener('loadedmetadata',() =>{
-        video.play()
-    })
-    videoGrid.append(video)
-    }//영상에 매체와 실행을 맡는 함수
+        video.play();
+    });
+    videoArea.append(video);
+    };
 
 
     function play() {
         var superBuffer = new Blob(recordedChunks);
-        videoElement.src =
-          window.URL.createObjectURL(superBuffer);
-      }
+        videoElement.src = window.URL.createObjectURL(superBuffer);
+      };
--- server.js
+++ server.js
@@ -1,46 +1,41 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
-let codeNum 
-let name
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
 
 
 
-app.set('view engine', 'ejs') // ejs 파일을 열수있게 한다.
-app.use(express.static('public')) 
+// ejs 파일을 열수있게 한다.
+app.set('view engine', 'ejs'); 
+app.use(express.static('public')); 
 
+// 경로가 "/" 일때 main.ejs가 열리고 타이틀에 이름을 변경
 app.get('/',(req,res) =>{
-    res.render("main",{title : "voiceChat"})
-}) //경로가 "/" 일때 main.ejs가 열리고 타이틀에 이름을 변경
+    res.render("main",{title : "voiceChat"});
+}); 
 
-
+// 경로가 "/voiceroom일때" 경로를 "/voiceroom= + 가상 키 "가 경로로 설정된다.
 app.get('/voiceroom',(req,res)=>{
-   
-    console.log(req.body)
-    res.redirect(`/voiceroom=${uuidV4()}`)
-}) //경로가 "/voiceroom일때" 경로를 "/voiceroom= + 가상 키 "가 경로로 설정된다. 
+    console.log(req.body);
+    res.redirect(`/voiceroom=${uuidV4()}`);
+});
 
+// 변경된 경로에 room에 파라미터 값을 roomId에 저장 시킨다.
 app.get('/voiceroom:room',(req,res)=>{
-    res.render('room',{ roomId: req.params.room})
-    console.log(req.body)
-}) //변경된 경로에 room에 파라미터 값을 roomId에 저장 시킨다.
+    res.render('room',{ roomId: req.params.room});
+}); 
 
-app.post('/confirm',(req,res,next) =>{
-    console.log(req.body)
-    res.render("confirm",{title: "CONFIRM"})
-})
-
+// 소켓 서버
 io.on('connection', socket =>{
     socket.on('join-room', (roomId, userId) =>{
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit("user-connected", userId)
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
 
         socket.on('disconnect',() =>{
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
-    })
-})
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        });
+    });
+});
 
-server.listen(3010,()=>console.log("connect port 3010"))
+server.listen(3010,()=>console.log("connect port 3010"));
